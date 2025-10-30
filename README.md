36f88681ce7cbcb66031824a334b18619899dcd7
ping1337 <mr8gudbits@gmail.com>
2025-10-30 16:16:40 +0600

    ping1337-theme v1.0
    ping1337-theme v1.0

---
+++ README.md
@@ -0,0 +1,11 @@
+# hfs-ping1337-theme
+
+---
+
+<div align="center">
+
+<h2>Screenshot</h2>
+
+![](./preview.png)
+
+## </div>
---
+++ dist/plugin.js
@@ -0,0 +1,7 @@
+exports.description = "ping1337 theme for 'rejetto/hfs'"
+exports.preview = "https://github.com/ping1337/hfs-ping1337-theme/raw/main/preview.png"
+exports.repo = "ping1337/ping1337-theme"
+exports.version = 1.0
+exports.isTheme = "dark"
+exports.apiRequired = 1
+exports.frontend_css = "style.css"
